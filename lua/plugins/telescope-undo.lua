ebaa0661bb22a0b201239b9112b22143e3681a33
Arpith Alexander <Arpith.Alexander@mobileye.com>
2025-11-01 20:36:02 +0100

    Add telescope-undo plugin

---
+++ lua/plugins/telescope-undo.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "debugloop/telescope-undo.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+      },
+    },
+    keys = {
+      { "<leader>U", "<cmd>Telescope undo<cr>", desc = "Telescope: Undo history" },
+    },
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          undo = {
+            use_delta = true,
+            side_by_side = true,
+            layout_strategy = "vertical",
+            layout_config = {
+              preview_height = 0.8,
+            },
+            mappings = {
+              i = {
+                ["<cr>"] = require("telescope-undo.actions").yank_additions,
+                ["<S-cr>"] = require("telescope-undo.actions").yank_deletions,
+                ["<C-cr>"] = require("telescope-undo.actions").restore,
+              },
+              n = {
+                ["y"] = require("telescope-undo.actions").yank_additions,
+                ["Y"] = require("telescope-undo.actions").yank_deletions,
+                ["u"] = require("telescope-undo.actions").restore,
+              },
+            },
+          },
+        },
+      })
+      require("telescope").load_extension("undo")
+    end,
+  },
+}
