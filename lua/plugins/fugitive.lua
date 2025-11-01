36ca1204577a953e3dd5c335ba42715748b025fe
Arpith Alexander <Arpith.Alexander@mobileye.com>
2025-11-01 19:01:16 +0100

    Add fugitive plugin

---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    "tpope/vim-fugitive",
+    cmd = { "Git", "G", "Gdiffsplit", "Gread", "Gwrite", "Ggrep", "GMove", "GDelete", "GBrowse", "GRemove", "GRename" },
+    -- Keymaps are now defined in lua/config/keymaps.lua
+  },
+}
